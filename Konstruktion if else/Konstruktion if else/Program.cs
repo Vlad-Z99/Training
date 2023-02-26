e6caec70d0f6b2afcde9e0ec8e61bd640663453e
Vladislav Zanfirov <126167687+Vlad-Z99@users.noreply.github.com>
2023-02-26 13:39:28 +0100

    Add files via upload

---
+++ Konstruktion if else/Konstruktion if else/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Konstruktion if else
+ */
+
+
+
+/*
+  
+                   if (Aussage oder Außerung)
+                    {
+                      Tat 1;
+                    }
+                   else
+                    {
+                      Tat 2;
+                    }                    
+ */ 
+ 
+namespace Konstruktion_if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Schreiben Sie Ihre Nummer");
+
+            a = int.Parse(Console.ReadLine());
+
+            
+
+            if (a % 2 == 0) 
+            {
+                Console.WriteLine("gerade Zahl");
+                
+            }
+            
+            else
+            {
+                Console.WriteLine("ungerade Zahl");
+                
+            }
+            
+
+        }
+        
+    }
+}
+
+
