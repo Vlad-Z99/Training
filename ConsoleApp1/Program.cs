65a6024a8272c16409963db313a163cd7d44c066
Vladislav Zanfirov <126167687+Vlad-Z99@users.noreply.github.com>
2023-02-26 12:25:28 +0100

    Beziehungsoperatoren

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/*
+ * Beziehungsoperatoren
+ * Vergleichsoperatoren
+ */
+
+/*
+                    == Gleich
+                    != Nicht Gleich
+                    < Weniger 
+                    > Großer
+                    <= Weniger oder Gleich
+                    >= Großer oder Gleich
+ */
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            
+            int b = 4;
+
+            bool result = a == b;
+
+            Console.WriteLine(a == b);
+
+            Console.WriteLine(a != b);
+
+            Console.WriteLine(a > b);
+
+            Console.WriteLine(a < b);
+
+            Console.WriteLine(a >= b);
+
+            Console.WriteLine(a <= b);
+        }
+    }
+}
